c123df24b552f8ae0ea1c9efefb68976f83aa3a5
danil-tr <daniltrvit@gmail.com>
2020-11-01 14:32:18 +0300

    1 version

--- task.c
+++ task.c
@@ -1,18 +1,50 @@
 /*
-System Calls: stat, fstat, statx
-
-в argv[1] помещаем название файла в текущей директории
-данная программа выведет информацию о файлах
-Пример: ./task task.c выведет информацию о данном файле.
+Работа с файлами: Запись в файл строку с клавиатуры.
+Создание двух файлов и работа уже с существующим
+argv[1] передаем имя файла
 */
 
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <assert.h>
 
-const int ERR_NO = 1;
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+const int SIZE = 1000;
+
+/*
+Функция для считывания строки с клавиатуры. Вовзращает указатель на строку.
+*/
+
+char* Input(int *length)
+{
+    int MAX_SIZE = 10;
+    int cur_length = 0;
+    *length = 0;
+
+    char* buffer = (char *) calloc(MAX_SIZE, sizeof(char));
+
+    while ( scanf("%10[^\n]%n", buffer + *length, &cur_length) != 0 )
+    {   
+        scanf ("%*1[\n]");
+        *length += cur_length;
+
+        if ( *length > MAX_SIZE/2 )
+        {
+            MAX_SIZE *= 2;
+
+            buffer = (char*) realloc(buffer, MAX_SIZE * sizeof(char) );
+        }
+    }
+    *(buffer + *length) = '\0';
+
+    return buffer;
+}
 
 /*
 Вводим строго два аргумента в консоли
@@ -21,44 +53,51 @@ void checkArgc(int a){
     assert (a == 2);
 }
 
-/*
-Создаем структуру и записываем в нее информацию. Находим ошибку если
-файл не существует
-*/
-int statCreate(const char* file_path, struct stat *buf, int line){
-    int flag = 0;
-
-    flag = stat( file_path, buf );
-
-    if (flag == -1)
-    {
-        fprintf (stderr, "There is no this file in the directory,"
-                         "error on line %d\n", line);
-        return -1;
-    }
-    else
-        return ERR_NO;
-
-}
 
 int main(int argc, char* argv[])
 {   
     checkArgc(argc);
+
+    /*Блок работы с консоль*/
+    int string_length = 0;
+    char *str = NULL;
+
+    printf("Enter the string that you want to write to the file...\n");
+    str = Input(&string_length);
+    printf("String: %s, length: %d\n", str, string_length);
     
-    const char* file_path = argv[1]; //указатель на имя файла из командной строки
+    /*Блок работы с файлами*/
 
-    struct stat file_stat;//структура, где храним данныео файле
-    if(statCreate(file_path, &file_stat, __LINE__) != ERR_NO)
-        return -1;
+    //Путевое имя файла
+    char *file_path = argv[1];
 
+    
+    mode_t mode = S_IRUSR | S_IWUSR| S_IRGRP | S_IWGRP | S_IROTH;//доступ на чтение и запись
+    /*
+    O_TRUNC приводит к очистке существующего файла. Данные, записываемые в файл,
+     замещают предыдущее содержимое файла.
+    O_APPEND приводит к открытию файла в режиме добавления. Данные, записываемые в
+    файл, добавляются в его конец.
+    O_CREAT означает создание нового файла. Если указанное имя соответствует
+    несуществующему файлу, он будет создан.
+    */
+    int file_flags = O_WRONLY | O_CREAT | O_APPEND;
 
-    printf("Information for file %s \n\n", file_path);
-    printf("File size: %ld bytes \n", file_stat.st_size);
-    printf("Number of links: %lu \n", file_stat.st_nlink);
-    printf("File inode: %lu \n", file_stat.st_ino);
-    printf("The file %s a symbolic link\n", (S_ISLNK(file_stat.st_mode)) ? "is" : "is not");
+    int file_des = open ( file_path, file_flags, mode );
+    if ( file_des == -1 )
+    {
+        fprintf(stderr, "Не удалось получить файловый дескриптор\n");
+        exit(1);
+    }
+    if( write(file_des, str, string_length) == -1 )
+    {
+        fprintf(stderr, "Не удалось записать строку в файл\n");
+        exit(1);
+    }
+    printf("Запись в файл %s c помощью write прошла успешно.\n", argv[1]);
 
-
+    close(file_des);
+    free(str);
 
     return 0;
 }
