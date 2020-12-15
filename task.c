cd774fe34049971fa2f3f7d804f211fc341bf945
danil-tr <daniltrvit@gmail.com>
2020-12-15 19:21:50 +0300

    1 verison

--- task.c
+++ task.c
@@ -1,73 +1,76 @@
 /*
-6.1 Вывод содержимого каталога.
-
+6.3 Вывод содержимого каталога.
 путь к каталогу в виде ./aa/bbb/
 аргумент строки - имя каталога
 */
-
+#include <dirent.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
 #include <assert.h>
-#include <dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/syscall.h> 
 #include <stdlib.h>
 #include <limits.h>
+#include <fcntl.h>
 
-const char* File_type ( int fd_dir, const char* path )
+char* checkType ( unsigned char type )
 {
-struct stat st;
-fstatat ( fd_dir, path, &st, 0 );
-if (S_ISLNK(st.st_mode))
+if ( type == DT_LNK ) 
 return "symbolic link";
 
-else if ( S_ISDIR(st.st_mode) )
+else if ( type == DT_DIR )
 return "directory";
 
-else if ( S_ISCHR(st.st_mode) )
+else if ( type == DT_CHR )
 return "character device";
 
-else if ( S_ISBLK(st.st_mode) )
+else if ( type == DT_BLK )
 return "block device";
 
-else if ( S_ISFIFO(st.st_mode) )
+else if ( type == DT_FIFO )
 return "fifo";
 
-else if ( S_ISSOCK(st.st_mode) )
+else if ( type == DT_SOCK )
 return "socket";
 
-else if ( S_ISREG(st.st_mode) )
+else if ( type == DT_REG )
 return "regular file";
 
 else
-assert ( "unknown file's type" );
-
+return  "unknown file's type";
 }
 
 
+struct linux_dirent {
+    long d_ino;
+    off_t d_off;
+    unsigned short d_reclen;
+    char d_name[];
+};
 
 int main(int argc, char* argv[])
 {   
-    char entry_path[PATH_MAX - 1] = {0};
+    const unsigned int BUF_SIZE = 1024;
+    char buf[BUF_SIZE];
     char *dir_path;
-    struct dirent *entry;
-    
-    if ( argc == 2 )
+    char entry_path[PATH_MAX - 1];
+    char dir_type;
+    struct linux_dirent *linux_dir;
+
+    if ( argc >= 2 )
     {
         dir_path = argv[1];
     } 
-    else if ( argc == 1 )
+    else
     {
         dir_path = ".";
     }
-    else
-    {
-        fprintf ( stderr, "argc error" );
-        exit (1);
-    }
-    
+
     size_t len_path = strlen ( dir_path );
+
     strncpy ( entry_path, dir_path, len_path );
     
     if ( entry_path[len_path - 1] != '/' )
@@ -77,33 +80,43 @@ int main(int argc, char* argv[])
         len_path++;
     }
 
-    DIR *dir = opendir ( dir_path );
-    assert ( dir != NULL );
-
-    int fd_dir = dirfd ( dir );
+    int fd_dir = open ( entry_path, O_RDONLY | O_DIRECTORY );
     assert ( fd_dir != -1 );
 
-    const char clear_part[PATH_MAX - 1] = {0};
+    int pos = 0;
 
-     while ( ( entry = readdir (dir) ) != NULL )
+    for ( ; ; )
     {
-        const char *type;
+        int read_bytes = syscall( SYS_getdents, fd_dir, buf, sizeof(char)*BUF_SIZE );
 
-        char *dir_part = entry->d_name;
-        size_t dir_part_len = strlen ( dir_part );
+        if ( read_bytes == -1)
+        {
+        fprintf ( stderr, "syscall getdents error\n" );
+        exit (1);
+        }
 
-        strncpy ( entry_path + len_path, dir_part, dir_part_len );
+        if ( read_bytes == 0 )
+        {
+            break;
+        }
 
-        type = File_type ( fd_dir, entry_path );
-        printf ( "%-18s: %s\n", type, entry_path );
-
-        strncpy ( entry_path + len_path, clear_part, dir_part_len );
+        printf("--------------- all bytes: = %d ---------------\n", read_bytes);
+        printf("i-node    file type    d_reclen    d_off    d_name\n");
         
-    }   
+        for ( pos = 0; pos < read_bytes; ) 
+        {
+            linux_dir = (struct linux_dirent *) (buf + pos);
 
+            printf("%9ld  ", linux_dir->d_ino);
 
-    close( fd_dir );		
-    closedir( dir );
+            dir_type = *(buf + pos + linux_dir->d_reclen - 1); // там хранится байт, отвечающий за тип в структуре
+            
+            printf("%-12s ", checkType ( dir_type ) );
+            printf("%5d %10lld  %s\n", linux_dir->d_reclen, (long long)linux_dir->d_off, linux_dir->d_name);
+            pos += linux_dir->d_reclen; // перемещаем указатель на следующую структуру
+        }
+        
+    }
 
     return 0;
 }
