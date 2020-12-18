a34cfac494db7c2953c37a3ea74bcdeaec78f930
danil-tr <daniltrvit@gmail.com>
2020-12-18 17:34:04 +0300

    1 version

--- task.c
+++ task.c
@@ -1,98 +1,62 @@
-
 /*
-6. Вывод содержимого каталога.
-
-путь к каталогу в виде ./aa/bbb/
-аргумент строки - имя каталога
+9 Вывод информации о дисковом пространстве
 */
 
-#include <stdio.h>
-#include <string.h>
-#include <assert.h>
 #include <dirent.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <sys/statvfs.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 
-#include <limits.h>
+const double GB_size = 1073741824;
 
-const char* File_type ( const char* path )
+const char* checkConsole (int argc_value, char* argv_value)
 {
-struct stat st;
-lstat(path, &st);
-if (S_ISLNK(st.st_mode))
-return "symbolic link";
+    char *path = NULL;
 
-else if ( S_ISDIR(st.st_mode) )
-return "directory";
-
-else if ( S_ISCHR(st.st_mode) )
-return "character device";
-
-else if ( S_ISBLK(st.st_mode) )
-return "block device";
-
-else if ( S_ISFIFO(st.st_mode) )
-return "fifo";
-
-else if ( S_ISSOCK(st.st_mode) )
-return "socket";
-
-else if ( S_ISREG(st.st_mode) )
-return "regular file";
-
-else
-assert ( "unknown file's type" );
-}
-
-int main(int argc, char* argv[])
-{   
-    char *dir_path; //строка с названием файла
-    
-    char entry_path[PATH_MAX - 1];//поместим имя файла с ограничением, установленным системой
-    if ( argc >= 2 )
+    if ( argc_value == 2 )
     {
-        dir_path = argv[1];
+        path = argv_value;
     } 
+    else if ( argc_value == 1 )
+    {
+        return "/";
+    }
     else
     {
-        dir_path = ".";//current direction
+        fprintf ( stderr, "argc error" );
+        exit (1);
     }
-    
-    /* Скопируем каталог и добавим недостающую часть */
 
-    size_t len_path = strlen ( dir_path );
+    size_t len_path = strlen ( path );
 
-    strncpy ( entry_path, dir_path, len_path );
-    
-    if ( entry_path[len_path - 1] != '/' )
+    if ( path[len_path - 1] != '/' )
     {
-        entry_path[len_path] = '/';
-        entry_path[len_path + 1] = '\0';
-        len_path++;
+        path[len_path] = '/';
+        path[len_path + 1] = '\0';
     }
-    
-    DIR *dir = opendir ( dir_path );
-    assert ( dir != NULL );
+    return path;
+}
 
-    struct dirent *entry; //указатель на структуру, которая хранит данные после readdir
+int main( int argc, char* argv[] )
+{
+    const char* path = checkConsole ( argc, argv[1] );
+    struct statvfs buf;
 
-    const char clear_part[PATH_MAX - 1] = {0};
-
-    while ( ( entry = readdir (dir) ) != NULL )
+    if ( statvfs ( path, &buf ) == -1 )
     {
-        const char *type;
-
-        char *dir_part = entry->d_name;
-        size_t dir_part_len = strlen ( dir_part );
-
-        strncpy ( entry_path + len_path, dir_part, dir_part_len );
-        type = File_type ( entry_path );
-        printf ( "%-18s: %s\n", type, entry_path );
-
-        strncpy ( entry_path + len_path, clear_part, dir_part_len );
+        printf ( "%s\n", strerror(errno) );
+        exit(1);
     }
-    closedir ( dir );
+    unsigned long frag_size = buf.f_frsize;
+    unsigned long fsys_size = frag_size * ( buf.f_blocks );
+    unsigned long avl_size = frag_size * ( buf.f_bfree );
     
+    printf ("---Disk capacity information---\n");
+    printf ("all space: %lf GB\n", fsys_size/GB_size );
+    printf ("available space: %lf GB\n", avl_size/GB_size );
+    printf ("used space: %lf GB\n", (fsys_size - avl_size)/GB_size );
+
     return 0;
 }
